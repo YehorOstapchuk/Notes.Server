59287063ab85b4b17519a321ec25e15fd2664cdc
Yehor Ostapchuk <cheat.book20@gmail.com>
2021-10-28 18:30:52 +0300

    Adding CreateNote Command and Handler for it

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -4,7 +4,7 @@ namespace Notes.Domain
 {
     public class Note
     {
-        public Guid UserID { get; set; }
+        public Guid UserId { get; set; }
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Details { get; set; }
