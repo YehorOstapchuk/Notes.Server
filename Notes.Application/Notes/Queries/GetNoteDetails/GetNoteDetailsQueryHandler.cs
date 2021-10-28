3632ec6261ac804aae9d576f8d37506388f05f4f
Yehor Ostapchuk <cheat.book20@gmail.com>
2021-10-28 19:28:31 +0300

    Adding DeleteNote and GetNoteDetails Command and Handler

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteDetailsQueryHandler
+        :IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INotesDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public GetNoteDetailsQueryHandler (INotesDbContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<NoteDetailsVm> Handle (GetNoteDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await dbContext.Notes
+                .FirstOrDefaultAsync(note =>
+                note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            return mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
