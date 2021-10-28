e000e57a4592edcda2399bff1013060f2ca52b71
Yehor Ostapchuk <cheat.book20@gmail.com>
2021-10-28 20:55:38 +0300

    Adding Notes Controller to WEb Api

---
+++ Notes.WebApi/Models/CreateNoteDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Notes.Commands.CreateNote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notes.WebApi.Models
+{
+    public class CreateNoteDto: IMapWith<CreateNoteCommand>
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateNoteDto, CreateNoteCommand>()
+                .ForMember(noteVm => noteVm.Title,
+                    opt => opt.MapFrom(note => note.Title))
+                .ForMember(noteVm => noteVm.Details,
+                    opt => opt.MapFrom(note => note.Details));
+        }
+    }
+}
