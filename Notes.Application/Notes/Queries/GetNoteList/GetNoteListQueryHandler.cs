0bedb0d6b68debca162ba5b5b3922a7ed20d2b81
Yehor Ostapchuk <cheat.book20@gmail.com>
2021-10-28 19:43:43 +0300

    Adding GetNoteList Query and Handler

---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryHandler
+        : IRequestHandler<GetNoteListQuery, NoteListVm>
+    {
+        private readonly INotesDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public GetNoteListQueryHandler(INotesDbContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
+        {
+            var notesQuery = await dbContext.Notes.Where(note => note.UserId == request.UserId)
+                                        .Where(note => note.UserId == request.UserId)
+                                        .ProjectTo<NoteLookUpDto>(mapper.ConfigurationProvider)
+                                        .ToListAsync(cancellationToken);
+
+            return new NoteListVm { Notes = notesQuery };
+        }
+
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/NoteLookUpDto.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class NoteLookUpDto : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteLookUpDto>()
+                .ForMember(noteVm => noteVm.Title,
+                    opt => opt.MapFrom(note => note.Title))
+                .ForMember(noteVm => noteVm.Id,
+                    opt => opt.MapFrom(note => note.Id));
+        }
+    }
+}
