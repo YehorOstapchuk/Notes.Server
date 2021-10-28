b4264bfc53d753ed5cfb9fc10c325d502108a110
Yehor Ostapchuk <cheat.book20@gmail.com>
2021-10-28 20:31:43 +0300

    Adding BaseController to Web Api

---
+++ Notes.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+
+namespace Notes.WebApi.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        private IMediator mediator;
+        protected IMediator Mediator =>
+            mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        internal Guid UserId => !User.Identity.IsAuthenticated
+            ? Guid.Empty
+            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+    }
+}
